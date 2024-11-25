c86d3106e21f79b3e18ac1940887e13d6b3d9ece
ALUNO SESI CABO <aluno.sesicabo@SISTEMAFIEPE.LOCAL>
2024-11-25 17:34:21 -0300

    make the system work

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Usuario(db.Model):
+    __tablename__ = 'usuarios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    create_data = db.Column(db.DateTime, default=db.func.current_timestamp())  
+
+    def __repr__(self):
+        return f'<Usuário {self.name}>'
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+
+    user = db.relationship('Usuario', backref=db.backref('posts', lazy=True))
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
