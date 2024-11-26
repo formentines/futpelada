88d73777f7f4f2ebbd6b80a2084dd6ba800e0f18
kchristian7 <christianmateuss17@gmail.com>
2024-11-26 17:33:44 -0300

    make the database work

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask
 from flask_mail import Mail
 from config import Config
 from models import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)  # Carregando configurações de e-mail e outras
+    
+    db.init_app(app)
+    
     mail = Mail(app)
 
     from . import routes
