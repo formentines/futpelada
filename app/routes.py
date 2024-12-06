27b15168339e5c2997066b698a418663ade51217
kchristian7 <christianmateuss17@gmail.com>
2024-12-06 00:33:16 -0300

    create new route for create_team

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from flask import render_template, request, redirect, url_for, flash
 from flask_mail import Message
 from app import db, mail
-from models import Usuario
+from models import User, Team
 from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash
+from werkzeug.utils import secure_filename
+import os
 
 serializer = URLSafeTimedSerializer('sua-chave-secreta')
 
@@ -18,6 +20,10 @@ def verify_token(token, expiration=3600):
         return email
     except (SignatureExpired, BadSignature):
         return None
+    
+def allow_file(filename):
+    allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
+    return '.' in filename.rsplit('.', 1)[1].lower() in allowed_extensions
     
 def init_app(app):
     @app.route('/')
@@ -36,7 +42,7 @@ def init_app(app):
             if not username or not email or not birth_date or not password or not confirm_password:
                 return "Todos os campos são obrigatórios!", 400
             
-            new_user = Usuario(username=username, email=email, birth_date=birth_date, password=password)
+            new_user = User(username=username, email=email, birth_date=birth_date, password=password)
 
             try:
                 db.session.add(new_user)
@@ -87,9 +93,9 @@ def init_app(app):
                 return redirect(url_for('forgotpassword'))
 
             if username:
-                user = Usuario.query.filter_by(username=username).first()
+                user = User.query.filter_by(username=username).first()
             elif email:
-                user = Usuario.query.filter_by(email=email).first()
+                user = User.query.filter_by(email=email).first()
             
             if user:
                 token = generate_token(user.email)
@@ -122,7 +128,7 @@ def init_app(app):
             password = request.form.get('SENHA')
             hashed_password = generate_password_hash(password)
 
-            user = Usuario.query.filter_by(email=email).first()
+            user = User.query.filter_by(email=email).first()
             if user:
                 user.password_hash = hashed_password
                 db.session.commit
@@ -131,3 +137,29 @@ def init_app(app):
                 return redirect(url_for('index'))
         
         return render_template('resetpassword'), token==token   
+    
+    @app.route('/create_team', methods = ['GET', 'POST'])
+    def createteam():
+        if request.method == ['POST']:
+            name = request.form.get('NOME DO TIME') 
+            description = request.form.get('DESCRIÇÃO')
+            social_media = request.form.get('REDES SOCIAIS')
+            state = request.form.get('ESTADO')
+            category = request.form.get('CATEGORIA')
+
+            image = request.files.get('SELECIONAR IMAGEM')
+            team_shield = None
+            if image:
+                if allow_file(image.filename):
+                    filename = secure_filename(image.filename)
+                    teamshield = os.path.json(app.config['UPLOAD_FOLDER'], filename)
+                    image.save(teamshield)
+            
+            new_team = Team(name=name, description=description, team_shield=teamshield, social_media=social_media, category=category, state=state)
+            
+            db.session.add(new_team)
+            db.session.commit()
+            
+            return redirect(url_for('index'))
+        
+        return render_template('create_team.html')
